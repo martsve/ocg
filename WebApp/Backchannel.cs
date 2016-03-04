fd72c6780be99771f5a62b49129e761ba3450c71
martsve <martsve@gmail.com>
2016-03-04 23:41:47 +0100

    Refactor out Backchannel

---
+++ WebApp/Backchannel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using Delver.Interface;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication1
+{
+    public static class Backchannel
+    {
+        private static IServerWithCallback _srv;
+
+        private static int msgNum = 0;
+
+        public static void Setup()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+
+            var cf =
+                new DuplexChannelFactory<IServerWithCallback>(
+                    new CallbackImpl(context),
+                    new NetTcpBinding(),
+                    new EndpointAddress("net.tcp://localhost:9080/DataService"));
+
+            _srv = cf.CreateChannel();
+        }
+
+        public static void Send(string cmd)
+        {
+            if (msgNum == 0)
+                _srv.StartNewServer();
+
+            else
+                _srv.SendCommand(1, 0, cmd);
+
+            msgNum++;
+        }
+
+        private class CallbackImpl : IGameCallback
+        {
+            private IHubContext Context { get; set; }
+
+            public CallbackImpl(IHubContext context)
+            {
+                Context = context;
+            }
+
+            public void SendDataPacket(string data)
+            {
+                Context.Clients.All.broadcastMessage("SERVER", data);
+            }
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,71 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
-using Delver.Interface;
-using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
 namespace WebApplication1
 {
-    public static class Backchannel
-    {
-        private static IServerWithCallback _srv;
-
-        private static int msgNum = 0;
-
-        public static void Setup()
-        {
-            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
-
-            var cf =
-                new DuplexChannelFactory<IServerWithCallback>(
-                    new CallbackImpl(context),
-                    new NetTcpBinding(),
-                    new EndpointAddress("net.tcp://localhost:9080/DataService"));
-
-            _srv = cf.CreateChannel();
-        }
-
-        public static void Send(string cmd)
-        {
-            if (msgNum == 0)
-                _srv.StartNewServer();
-
-            else
-                _srv.SendCommand(1, 0, cmd);
-
-            msgNum++;
-        }
-
-        private class CallbackImpl : IGameCallback
-        {
-            private IHubContext Context { get; set; }
-
-            public CallbackImpl(IHubContext context)
-            {
-                Context = context;
-            }
-
-            public void SendDataPacket(string data)
-            {
-                Context.Clients.All.broadcastMessage("SERVER", data);
-            }
-        }
-    }
-
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            // Set up WCF to ServiceHost
             Backchannel.Setup();
+
+            // Set up SignalR 
             app.MapSignalR();
+
+            // Configure some WebApp stuff..?
             ConfigureAuth(app);
         }
-
-
     }
 }
